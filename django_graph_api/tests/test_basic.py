f954377b98b84acbe5c18cafc11a4087691db777
Stephen Burrows <stephen.r.burrows@gmail.com>
2017-10-10 22:20:05 -0700

    Simplified folder structure
    Moved test app and test projects to repo root.

--- django_graph_api/tests/test_basic.py
+++ django_graph_api/tests/test_basic.py
@@ -1,7 +1,7 @@
-from ..schema import schema
+from test_app.schema import schema
 
 
-def test_hero_name():
+def test_hero_name(starwars_data):
     document = '''
     {
         hero {
@@ -18,7 +18,7 @@ def test_hero_name():
     }
 
 
-def test_hero_name_and_friends_names():
+def test_hero_name_and_friends_names(starwars_data):
     document = '''
     {
         hero {
@@ -52,7 +52,7 @@ def test_hero_name_and_friends_names():
     }
 
 
-def test_hero_name_and_episodes():
+def test_hero_name_and_episodes(starwars_data):
     document = '''
     {
         hero {
