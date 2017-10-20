40cbc27ab5c5e4c16f22bc1d3d422e625e9d6a2a
Arianne Dee <ariannedee@gmail.com>
2017-10-19 18:09:30 -0700

    Fix flake8 errors

--- django_graph_api/graphql/helpers.py
+++ django_graph_api/graphql/helpers.py
@@ -1,3 +1,3 @@
 def is_lambda(object_type):
-    a_lambda = lambda: 0
+    a_lambda = lambda: 0  # noqa
     return isinstance(object_type, type(a_lambda)) and object_type.__name__ == a_lambda.__name__
--- docs/conf.py
+++ docs/conf.py
@@ -31,11 +31,13 @@ sys.path.insert(0, os.path.abspath('../django_graph_api/graphql'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
+extensions = [
+    'sphinx.ext.autodoc',
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
     'sphinx.ext.viewcode',
-    'sphinx.ext.intersphinx']
+    'sphinx.ext.intersphinx'
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -172,7 +174,6 @@ texinfo_documents = [
      author, 'django-graph-api', 'One line description of project.',
      'Miscellaneous'),
 ]
-
 
 
 # -- Options for Epub output ----------------------------------------------
