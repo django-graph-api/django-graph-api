596fa325a490fc458f9abb5c557fe3d03c642489
Shu Shen <shu.shen@gmail.com>
2018-01-23 21:40:42 -0800

    Pass admin.site.urls directly to url()
    This fixes the deprecated warning "Passing a 3-tuple to
    django.conf.urls.include() is deprecated." per [1].
    
    [1] https://docs.djangoproject.com/en/1.11/releases/1.9/#passing-a-3-tuple-or-an-app-name-to-include
    
    Signed-off-by: Shu Shen <shu.shen@gmail.com>

--- test_project/urls.py
+++ test_project/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import include, url
+from django.conf.urls import url
 from django.contrib import admin
 
 from django_graph_api.views import GraphQLView
@@ -6,6 +6,6 @@ from test_app.schema import schema
 
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
     url(r'^graphql$', GraphQLView.as_view(schema=schema)),
 ]
